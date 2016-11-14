36f347e3ab18c23bf76b5301283b9151eb112436
Stephen Connolly <stephen.alan.connolly@gmail.com>
2016-11-14 22:27:15 +0000

    Update the MethodUtils helper implementation

--- src/main/java/jenkins/scm/api/MethodUtils.java
+++ src/main/java/jenkins/scm/api/MethodUtils.java
@@ -1,45 +1,66 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * The MIT License
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright (c) 2016 CloudBees, Inc.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  */
 
 package jenkins.scm.api;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Nonnull;
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.ClassUtils;
 import org.apache.commons.lang.Validate;
 
 /**
- * The bits of commons-lang3 MethodUtils that we need to check if protected methods have been overridden.
+ * Helper to identify methods that have not been implemented / overridden.
  */
 class MethodUtils {
+    /**
+     * Checks if the method is abstract or not.
+     * @param clazz the class.
+     * @param methodName the method name.
+     * @param parameterTypes the parameter types.
+     * @return {@code true} if the method does not exist or is abstract.
+     */
+    static boolean isAbstract(Class<?> clazz, String methodName, Class<?>... parameterTypes) {
+        Method m = getMethodImpl(clazz, methodName, parameterTypes);
+        return m == null || Modifier.isAbstract(m.getModifiers());
+    }
+
     /**
      * Checks if the  method defined on the base type with the given arguments
      * are overridden in the given derived type.
      */
-    public static boolean isOverridden(@Nonnull Class base, @Nonnull Class derived, @Nonnull String methodName,
+    static boolean isOverridden(@Nonnull Class base, @Nonnull Class derived, @Nonnull String methodName,
                                        @Nonnull Class... types) {
-        Method baseMethod = getMatchingMethod(base, methodName, types);
-        Method derivedMethod = getMatchingMethod(derived, methodName, types);
-        return baseMethod == null ? derivedMethod != null : !baseMethod.equals(derivedMethod);
+        Method baseMethod = getMethodImpl(base, methodName, types);
+        Method derivedMethod = getMethodImpl(derived, methodName, types);
+        return baseMethod == null ?
+                derivedMethod != null && !Modifier.isAbstract(derivedMethod.getModifiers())
+                : !baseMethod.equals(derivedMethod);
     }
-
-    // Remaining methods
 
     /**
      * <p>Retrieves a method whether or not it's accessible. If no such method
@@ -50,63 +71,60 @@ class MethodUtils {
      * @param parameterTypes Argument class types
      * @return The method
      */
-    public static Method getMatchingMethod(final Class<?> cls, final String methodName,
-                                           final Class<?>... parameterTypes) {
+    static Method getMethodImpl(final Class<?> cls, final String methodName,
+                                final Class<?>... parameterTypes) {
         Validate.notNull(cls, "Null class not allowed.");
         Validate.notEmpty(methodName, "Null or blank methodName not allowed.");
 
-        // Address methods in superclasses
-        Method[] methodArray = cls.getDeclaredMethods();
-        final List<Class<?>> superclassList = ClassUtils.getAllSuperclasses(cls);
-        for (final Class<?> klass : superclassList) {
-            methodArray = (Method[])ArrayUtils.addAll(methodArray, klass.getDeclaredMethods());
-        }
-
-        Method inexactMatch = null;
-        for (final Method method : methodArray) {
+        // fast path, check if directly declared on the class itself
+        for (final Method method : cls.getDeclaredMethods()) {
             if (methodName.equals(method.getName()) &&
-                    ArrayUtils.isEquals(parameterTypes, method.getParameterTypes())) {
+                    Arrays.equals(parameterTypes, method.getParameterTypes())) {
                 return method;
-            } else if (methodName.equals(method.getName()) &&
-                    ClassUtils.isAssignable(parameterTypes, method.getParameterTypes(), true)) {
-                if (inexactMatch == null) {
-                    inexactMatch = method;
-                } else if (distance(parameterTypes, method.getParameterTypes())
-                        < distance(parameterTypes, inexactMatch.getParameterTypes())) {
-                    inexactMatch = method;
+            }
+        }
+        if (!cls.isInterface()) {
+            // ok, now check if directly implemented on a superclass
+            // Java 8: note that super-interface implementations trump default methods
+            for (Class<?> klass = cls.getSuperclass(); klass != null; klass = klass.getSuperclass()) {
+                for (final Method method : klass.getDeclaredMethods()) {
+                    if (methodName.equals(method.getName()) &&
+                            Arrays.equals(parameterTypes, method.getParameterTypes())) {
+                        return method;
+                    }
                 }
             }
-
         }
-        return inexactMatch;
-    }
+        // ok, now we are looking for an interface method... the most specific one
+        // in the event that we have two unrelated interfaces both declaring a method of the same name
+        // we will give up and say we could not find the method (the logic here is that we are primarily
+        // checking for overrides, in the event of a Java 8 default method, that default only
+        // applies if there is no conflict from an unrelated interface... thus if there are
+        // default methods and they are unrelated then they don't exist... if there are multiple unrelated
+        // abstract methods... well they won't count as a non-abstract implementation
+        Method res = null;
+        for (final Class<?> klass : (List<Class<?>>)ClassUtils.getAllInterfaces(cls)) {
+            for (final Method method : klass.getDeclaredMethods()) {
+                if (methodName.equals(method.getName()) &&
+                        Arrays.equals(parameterTypes, method.getParameterTypes())) {
+                    if (res == null) {
+                        res = method;
+                    } else {
+                        Class<?> c = res.getDeclaringClass();
+                        if (c == klass) {
+                            // match, ignore
+                        } else if (c.isAssignableFrom(klass)) {
+                            // this is a more specific match
+                            res = method;
+                        } else if (!klass.isAssignableFrom(c)) {
+                            // multiple overlapping interfaces declare this method and there is no common ancestor
+                            return null;
 
-    /**
-     * <p>Returns the aggregate number of inheritance hops between assignable argument class types.  Returns -1
-     * if the arguments aren't assignable.  Fills a specific purpose for getMatchingMethod and is not generalized.</p>
-     *
-     * @param classArray
-     * @param toClassArray
-     * @return the aggregate number of inheritance hops between assignable argument class types.
-     */
-    private static int distance(final Class<?>[] classArray, final Class<?>[] toClassArray) {
-        int answer = 0;
-
-        if (!ClassUtils.isAssignable(classArray, toClassArray, true)) {
-            return -1;
-        }
-        for (int offset = 0; offset < classArray.length; offset++) {
-            // Note InheritanceUtils.distance() uses different scoring system.
-            if (classArray[offset].equals(toClassArray[offset])) {
-                continue;
-            } else if (ClassUtils.isAssignable(classArray[offset], toClassArray[offset], true)
-                    && !ClassUtils.isAssignable(classArray[offset], toClassArray[offset], false)) {
-                answer++;
-            } else {
-                answer = answer + 2;
+                        }
+                    }
+                }
             }
         }
-
-        return answer;
+        return res;
     }
 }
