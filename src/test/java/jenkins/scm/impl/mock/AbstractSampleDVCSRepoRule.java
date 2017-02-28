bbae0c55ee093d5539aafc55b8a59841b13311d4
Jesse Glick <jglick@cloudbees.com>
2017-02-28 15:27:18 -0500

    Moving AbstractSampleDVCSRepoRule & AbstractSampleRepoRule from workflow-scm-step since they are more broadly applicable.

---
+++ src/test/java/jenkins/scm/impl/mock/AbstractSampleDVCSRepoRule.java
@@ -0,0 +1,99 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.scm.impl.mock;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+import static jenkins.scm.impl.mock.AbstractSampleRepoRule.run;
+
+/**
+ * Rule tailored to a DVCS which may be initialized and cloned from a local directory.
+ */
+public abstract class AbstractSampleDVCSRepoRule extends AbstractSampleRepoRule {
+
+    /**
+     * The origin clone.
+     */
+    protected File sampleRepo;
+
+    @Override protected void before() throws Throwable {
+        super.before();
+        sampleRepo = tmp.newFolder();
+    }
+
+    /**
+     * Write a file to {@link #sampleRepo}.
+     * @param rel relative path ({@code /}-separated)
+     * @param text text to write
+     */
+    public final void write(String rel, String text) throws IOException {
+        FileUtils.write(new File(sampleRepo, rel), text);
+    }
+
+    /**
+     * @return path of {@link #sampleRepo}
+     */
+    @Override public final String toString() {
+        return sampleRepo.getAbsolutePath();
+    }
+
+    /**
+     * Initialize a repository in an empty directory.
+     */
+    public abstract void init() throws Exception;
+
+    /**
+     * Run the specified tool inside {@link #sampleRepo}.
+     * @param tool a tool like {@code git}
+     * @param cmds its arguments
+     */
+    protected final void run(String tool, String... cmds) throws Exception {
+        List<String> args = new ArrayList<String>();
+        args.add(tool);
+        args.addAll(Arrays.asList(cmds));
+        run(false, sampleRepo, args.toArray(new String[args.size()]));
+    }
+
+    /**
+     * Like {@link #fileUrl} but expressed only as a path, not a URL with protocol.
+     */
+    public final String bareUrl() throws UnsupportedEncodingException {
+        return URLEncoder.encode(toString(), "UTF-8");
+    }
+
+    /**
+     * {@code file}-protocol URL to {@link #sampleRepo}.
+     */
+    public final String fileUrl() {
+        return sampleRepo.toURI().toString();
+    }
+
+}
---
+++ src/test/java/jenkins/scm/impl/mock/AbstractSampleRepoRule.java
@@ -0,0 +1,91 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.scm.impl.mock;
+
+import hudson.Launcher;
+import hudson.model.TaskListener;
+import hudson.triggers.SCMTrigger;
+import hudson.util.StreamTaskListener;
+import java.io.File;
+import java.util.Arrays;
+import static org.hamcrest.Matchers.is;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.AssumptionViolatedException;
+import org.junit.Rule;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.TemporaryFolder;
+import org.jvnet.hudson.test.JenkinsRule;
+
+/**
+ * Basis for {@link Rule} classes which run a concrete SCM tool on a sample repository, for integration testing.
+ */
+public abstract class AbstractSampleRepoRule extends ExternalResource {
+
+    /**
+     * Run a given command.
+     * @param probing if true, throw {@link AssumptionViolatedException} rather than {@link AssertionError} in case of problems
+     * @param cwd working directory to use
+     * @param cmds command and arguments to run
+     */
+    public static void run(boolean probing, File cwd, String... cmds) throws Exception {
+        try {
+            TaskListener listener = StreamTaskListener.fromStdout();
+            int r = new Launcher.LocalLauncher(listener).launch().cmds(cmds).pwd(cwd).stdout(listener).join();
+            String message = Arrays.toString(cmds) + " failed with error code";
+            if (probing) {
+                Assume.assumeThat(message, r, is(0));
+            } else {
+                Assert.assertThat(message, r, is(0));
+            }
+        } catch (Exception x) {
+            if (probing) {
+                Assume.assumeNoException(Arrays.toString(cmds) + " failed with exception (required tooling not installed?)", x);
+            } else {
+                throw x;
+            }
+        }
+    }
+
+    protected final TemporaryFolder tmp;
+
+    protected AbstractSampleRepoRule() {
+        this.tmp = new TemporaryFolder();
+    }
+
+    @Override protected void before() throws Throwable {
+        tmp.create();
+    }
+
+    @Override protected void after() {
+        tmp.delete();
+    }
+
+    /** Otherwise {@link JenkinsRule#waitUntilNoActivity()} is ineffective when we have just pinged a commit notification endpoint. */
+    protected final void synchronousPolling(JenkinsRule r) {
+        r.jenkins.getDescriptorByType(SCMTrigger.DescriptorImpl.class).synchronousPolling = true;
+    }
+
+}
