bb084af5d497cc68df19f65f8ba019dd0fb32099
Stephen Connolly <stephen.alan.connolly@gmail.com>
2017-11-03 14:22:57 +0000

    Add an API to help normalise URLs related to SCMs

---
+++ src/main/java/jenkins/scm/api/SCMUri.java
@@ -0,0 +1,227 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.scm.api;
+
+import edu.umd.cs.findbugs.annotations.CheckForNull;
+import java.net.IDN;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Utility class to help with SCM URI related things.
+ *
+ * @since 2.2.5
+ */
+public final class SCMUri {
+
+    /**
+     * The default ports for common URI schemes that it is safe to perform hostname normalization on.
+     */
+    private static final Map<String, Integer> commonDefaultPorts;
+
+    /**
+     * Utility class
+     */
+    private SCMUri() {
+        throw new IllegalAccessError("Utility class");
+    }
+
+    static {
+        Map<String, Integer> ports = new HashMap<String, Integer>();
+        ports.put("ftp", 21);
+        ports.put("ssh", 22);
+        ports.put("telnet", 22);
+        ports.put("http", 80);
+        ports.put("https", 443);
+        commonDefaultPorts = Collections.unmodifiableMap(ports);
+    }
+
+    /**
+     * Returns the common default ports.
+     *
+     * @return the common default ports.
+     */
+    public static Map<String, Integer> commonDefaultPorts() {
+        return commonDefaultPorts;
+    }
+
+    /**
+     * Normalize an URI. Normalization will apply the following changes:
+     * <ul>
+     * <li>The scheme will be forced to lowercase (scheme is case insensitive, so should not matter)</li>
+     * <li>For schemes that use a hostname or user authority plus hostname format (such as {@code ssh},
+     * {@code http} and {@code https} The hostname will be converted from IDNA to ASCII if necessary and forced to
+     * lowercase and the PCT-encoding of the user authority will be enforced (note that this may affect the case where
+     * a username in a user authority contains a {@code :} as the {@link URI} class does not provide a means to
+     * destructure and restructure a URI without encoding/decoding the username, consequently the {@code %3A} will
+     * be converted to {@code :} and not converted back)</li>
+     * <li>Any redundant path navigation segments will be removed, for example a path of {@code /foo/../bar} will
+     * be normalized to {@code /bar}</li>
+     * <li>Any trailing {@code /} will be removed</li>
+     * </ul>
+     *
+     * @param uri the URI.
+     * @return the normalized api URI.
+     */
+    @CheckForNull
+    public static String normalize(@CheckForNull String uri) {
+        return normalize(uri, commonDefaultPorts);
+    }
+
+    /**
+     * Normalize an URI. Normalization will apply the following changes:
+     * <ul>
+     * <li>The scheme will be forced to lowercase (scheme is case insensitive, so should not matter)</li>
+     * <li>For schemes in the supplied default ports that use a hostname or user authority plus hostname format (such
+     * as {@code ssh},
+     * {@code http} and {@code https} The hostname will be converted from IDNA to ASCII if necessary and forced to
+     * lowercase and the PCT-encoding of the user authority will be enforced (note that this may affect the case where
+     * a username in a user authority contains a {@code :} as the {@link URI} class does not provide a means to
+     * destructure and restructure a URI without encoding/decoding the username, consequently the {@code %3A} will
+     * be converted to {@code :} and not converted back)</li>
+     * <li>Any redundant path navigation segments will be removed, for example a path of {@code /foo/../bar} will
+     * be normalized to {@code /bar}</li>
+     * <li>Any trailing {@code /} will be removed</li>
+     * </ul>
+     *
+     * @param uri the URI
+     * @param defaultPorts the URI scheme to default port mapping (a varargs list to make combination easier). Normally
+     *                     the caller would use {@link #commonDefaultPorts()} as one of the arguments.
+     * @return the normalized api URI.
+     */
+    @CheckForNull
+    public static String normalize(@CheckForNull String uri, Map<String, Integer>... defaultPorts) {
+        if (uri == null) {
+            return null;
+        }
+        try {
+            URI u = new URI(uri).normalize();
+            String scheme = u.getScheme().toLowerCase(Locale.ENGLISH);
+            Integer defaultPort = null;
+            for (Map<String, Integer> ports : defaultPorts) {
+                defaultPort = ports.get(scheme);
+                if (defaultPort != null) {
+                    break;
+                }
+            }
+            if (defaultPort != null) {
+                // this is a scheme where we know the authority is server based, i.e. [userinfo@]server[:port]
+                // DNS names must be US-ASCII and are case insensitive, so we force all to lowercase
+
+                String host = u.getHost();
+                if (host != null) {
+                    host = host.toLowerCase(Locale.ENGLISH);
+                    int port = u.getPort();
+                    uri = new URI(
+                            scheme,
+                            u.getUserInfo(),
+                            host,
+                            port == defaultPort ? -1 : port,
+                            u.getPath(),
+                            u.getQuery(),
+                            u.getFragment()
+                    ).toASCIIString();
+                } else {
+                    String authority = u.getAuthority();
+                    if (authority != null) {
+                        int at = authority.indexOf('@');
+                        int colon = authority.lastIndexOf(':');
+                        if (colon != -1 && at > colon) {
+                            // if the @ is after the last : then the : is not prefixing a port, rather it is probably
+                            // the password
+                            colon = -1;
+                        }
+                        if (colon != -1) {
+                            for (int i = colon + 1; i < authority.length(); i++) {
+                                if (!Character.isDigit(authority.codePointAt(i))) {
+                                    colon = -1;
+                                    break;
+                                }
+                            }
+                        }
+                        String userPart;
+                        String userSep;
+                        String hostPart;
+                        String portSep;
+                        String portPart;
+                        if (at == -1) {
+                            userPart = "";
+                            userSep = "";
+                        } else {
+                            userPart = authority.substring(0, at);
+                            userSep = "@";
+                        }
+                        if (colon == -1) {
+                            portSep = "";
+                            portPart = "";
+                        } else {
+                            int port = Integer.parseInt(authority.substring(colon + 1));
+                            if (port != defaultPort) {
+                                portSep = ":";
+                                portPart = Integer.toString(port);
+                            } else {
+                                portSep = "";
+                                portPart = "";
+                            }
+                        }
+                        hostPart = IDN.toASCII(authority.substring(at + 1, colon == -1 ? authority.length() : colon))
+                                .toLowerCase(Locale.ENGLISH);
+                        authority = userPart + userSep + hostPart + portSep + portPart;
+                    }
+                    uri = new URI(scheme,
+                            authority,
+                            u.getPath(), u.getQuery(),
+                            u.getFragment()).toASCIIString();
+                }
+            } else {
+                String host = u.getHost();
+                if (u.getHost() != null) {
+                    host = host.toLowerCase(Locale.ENGLISH);
+                    uri = new URI(
+                            scheme,
+                            u.getUserInfo(),
+                            host,
+                            u.getPort(),
+                            u.getPath(),
+                            u.getQuery(),
+                            u.getFragment()
+                    ).toASCIIString();
+                } else {
+                    uri = new URI(scheme,
+                            u.getSchemeSpecificPart(),
+                            u.getFragment()).toASCIIString();
+                }
+            }
+        } catch (URISyntaxException e) {
+            // ignore, this was a best effort tidy-up
+        }
+        return uri.replaceAll("/$", "");
+    }
+
+}
---
+++ src/test/java/jenkins/scm/api/SCMUriTest.java
@@ -0,0 +1,105 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2017 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package jenkins.scm.api;
+
+import java.util.Collections;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SCMUriTest {
+    @Test
+    public void given__url_with_custom_scheme__when__normalizing__then__custom_default_port_elided() throws Exception {
+        assertThat(SCMUri.normalize("myscm://myscm.EXAMPLE.COM:6352/", Collections.singletonMap("myscm", 6352)), is("myscm://myscm.example.com"));
+        assertThat(SCMUri.normalize("myscm://myscm.EXAMPLE.COM:6352/"), is("myscm://myscm.example.com:6352"));
+    }
+
+    @Test
+    public void given__url_without_hostname__when__normalizing__then__no_name_inferred() throws Exception {
+        assertThat(SCMUri.normalize("FILE:///some/random/file"), is("file:///some/random/file"));
+    }
+
+    @Test
+    public void given__normalized_url__when__normalizing__then__verbatim() throws Exception {
+        assertThat(SCMUri.normalize("https://example.com/foo"), is("https://example.com/foo"));
+        assertThat(SCMUri.normalize("https://example.com:8443/foo"), is("https://example.com:8443/foo"));
+        assertThat(SCMUri.normalize("https://bob@example.com/foo"), is("https://bob@example.com/foo"));
+        assertThat(SCMUri.normalize("https://bob:pass@example.com/foo"), is("https://bob:pass@example.com/foo"));
+        assertThat(SCMUri.normalize("https://bob%25smith@example.com/foo"), is("https://bob%25smith@example.com/foo"));
+        assertThat(SCMUri.normalize("https://bob%25smith:pass@example.com/foo"),
+                is("https://bob%25smith:pass@example.com/foo"));
+    }
+
+    @Test
+    public void given__url_with_punycode__when__normalizing__then__punycode_retained() throws Exception {
+        assertThat(SCMUri.normalize("http://xn--e1afmkfd.xn--p1ai/"),
+                is("http://xn--e1afmkfd.xn--p1ai" /*http://пример.рф*/));
+    }
+
+    @Test
+    public void given__url_with_idn__when__normalizing__then__hostname_is_decoded() throws Exception {
+        assertThat(SCMUri.normalize("http://\u043F\u0440\u0438\u043C\u0435\u0440.\u0440\u0444/"), /*пример.рф*/
+                is("http://xn--e1afmkfd.xn--p1ai"));
+    }
+
+    @Test
+    public void given__url_with_idn_and_port__when__normalizing__then__hostname_is_decoded() throws Exception {
+        assertThat(SCMUri.normalize("http://\u043F\u0440\u0438\u043C\u0435\u0440.\u0440\u0444:8080/"), /*пример.рф*/
+                is("http://xn--e1afmkfd.xn--p1ai:8080"));
+    }
+
+    @Test
+    public void given__url_with_user_and_idn__when__normalizing__then__hostname_is_decoded() throws Exception {
+        assertThat(SCMUri.normalize("http://bob@\u043F\u0440\u0438\u043C\u0435\u0440.\u0440\u0444:8080/"), /*пример.рф*/
+                is("http://bob@xn--e1afmkfd.xn--p1ai:8080"));
+    }
+
+    @Test
+    public void given__url_with_userpass_and_idn__when__normalizing__then__hostname_is_decoded() throws Exception {
+        assertThat(SCMUri.normalize("http://bob:secret@пример.рф/"),
+                is("http://bob:secret@xn--e1afmkfd.xn--p1ai"));
+    }
+
+    @Test
+    public void given__url_with_userpass_and_idn_and_port__when__normalizing__then__hostname_is_decoded() throws Exception {
+        assertThat(SCMUri.normalize("http://bob:secret@пример.рф:8080/"),
+                is("http://bob:secret@xn--e1afmkfd.xn--p1ai:8080"));
+    }
+
+    @Test
+    public void given__url_with_everything_messed_up__when__normalizing__then__everything_is_fixed() throws Exception {
+        // the scheme is upper case, we expect that to be fixed
+        // the user part should be ...
+        // the host is IDN so that should be fixed
+        // the port here is ๔๔๓ which is the Thai digits for 443, and as the scheme is https
+        // the path should be normalized
+        // the trailing slash should be removed
+        assertThat(SCMUri.normalize(
+                "HTTPS://böb:seçret@\u043F\u0440\u0438\u043C\u0435\u0440.\u0440\u0444:\u0E54\u0E54\u0E53/foo/../"),
+                is("https://b%C3%B6b:se%C3%A7ret@xn--e1afmkfd.xn--p1ai"));
+    }
+
+}
